1278b7d58afeb988f0ea52e06895d87241c9da45
coding-sta <personal.sta.bd@gmail.com>
2022-05-04 05:46:18 +0600

    delivered

--- src/Pages/CarUpdate/CarUpdate.js
+++ src/Pages/CarUpdate/CarUpdate.js
@@ -1,20 +1,38 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import useCars from '../../Hooks/useCars';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 const CarUpdate = () => {
-    const {_id}=useParams()
-    console.log(useParams())
-    const [car,setCar]=useState({})
-    useEffect(()=>{
-        fetch(`http://localhost:5000/car/${_id}`)
-        .then(res=>res.json())
-        .then(data=>setCar(data))
-    },[])
-    return (
-        <div>
-            <h1>car{car.name}</h1>
-        </div>
-    );
+  const { _id } = useParams();
+  const [car, setCar] = useState({});
+  useEffect(() => {
+    fetch(`http://localhost:5000/car/${_id}`)
+      .then((res) => res.json())
+      .then((data) => setCar(data));
+  }, [_id]);
+
+  const delivered = () => {
+    const quantity = parseInt(car.quantity) - 1;
+    const newQuantity={quantity}
+    fetch(`http://localhost:5000/car/${_id}`, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(newQuantity),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
+
+  return (
+    <div>
+      <h1>car{car.name}</h1>
+
+      <h1>quantity : {car.quantity}</h1>
+      <button onClick={delivered}>delivered</button>
+    </div>
+  );
 };
 
 export default CarUpdate;
